7d65ea49eff4235d412612b75e18e5281b961195
AndroidDoctorr <gbartthenerd@gmail.com>
2023-08-26 17:43:30 -0400

    trying a different stragegy to show images

--- src/App.jsx
+++ src/App.jsx
@@ -105,7 +105,7 @@ function App() {
           <a href="https://store.steampowered.com/app/2216560/Bubble_Siege/">
           <h2 className='centerText'>
             Bubble Siege
-            <img className='game-thumb' src={bsIcon}></img>
+            <img className='game-thumb' src={'./assets/bs-thumb.png'}></img>
           </h2>
           </a>
         </div>
